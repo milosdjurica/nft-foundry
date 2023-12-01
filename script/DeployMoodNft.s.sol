25cde9de871d62f8faefeffafdd5d72e454a50f1
milosdjurica <milosdjurica13@gmail.com>
2023-12-01 17:07:16 +0100

    deploy mood nft and tests

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -1,12 +1,25 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-import {Script} from "../lib/forge-std/src/Script.sol";
+import {Script, console} from "../lib/forge-std/src/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./images/svg/sad.svg");
+        string memory happySvg = vm.readFile("./images/svg/happy.svg");
+        // console.log(sadSvg);
+        // console.log(happySvg);
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageURI(
         string memory svg
