1aa5c7bd319c9587a9b5f5dc0d9923949acfdedd
milosdjurica <milosdjurica13@gmail.com>
2023-12-01 01:39:29 +0100

    named imports

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
     uint private s_tokenCounter;
